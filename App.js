36ec26c278b4e88a986a5f383cde72d9753a1545
Riski Rahmat Hia <riskirahmathia3@gmail.com>
2023-11-07 21:48:32 +0700

    add counter component and state

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import Tabs from './src/components/Tabs'
+import Counter from './src/demonstration/Counter'
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tabs />
+      <Counter />
     </NavigationContainer>
   )
 }
